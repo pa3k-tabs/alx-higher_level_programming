cdc666bd5dd9d4a8f12b176cc1f2289372f5d988
Epor Patrick <epoxide7@gmail.com>
2023-06-06 05:51:21 -0700

    abc

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,7 +3,14 @@
 
 #include <stdlib.h>
 
-
+/**
+ *  * struct listint_s - singly linked list
+ *   * @n: integer
+ *    * @next: points to the next node
+ *     *
+ *      * Description: singly linked list node structure
+ *       * for Holberton project
+ *        */
 typedef struct listint_s
 {
 		int n;
@@ -15,4 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif /* LISTS_H /*
+/
