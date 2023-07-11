2be7e8f2548f1eadcc146eccbc61adb553b4c0ba
Epor Patrick <epoxide7@gmail.com>
2023-07-11 13:39:34 -0700

    bnm

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Representation of a student"""
+    """Init the class."""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """Retrieves a dictionary representation of a Student instance.
+        Args:
+            1. attrs: The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(att) == str for att in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
